630955c219fb5a4ec8d936ff072db0e4516139b2
Chris Kerr <ckerr@chorus.one>
2025-05-09 14:29:17 +0300

    Switch to older Nixpkgs version to get compatible 'cryptography' package

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "Python library for the YubiHSM 2";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    # Starting from 22.11 we get errors about missing `cryptography.utils.register_interface` decorator
+    nixpkgs.url = "nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
